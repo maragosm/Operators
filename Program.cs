dd8b46398f2911f5fe22547fae6ecf459a3fc9ad
Michael Maragos <maragosm@aol.com>
2022-06-01 10:58:07 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 17;
+int b = 4;
+int quotient = a / b;
+int remainder = a % b;
+
+Console.WriteLine($"17 / 4 is {quotient} remainder {remainder}");
